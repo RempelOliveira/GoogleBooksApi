46d086556583ede3c7e8d6c5a834c9a1fd3fcba2
Guilherme Rempel de Oliveira <rempel.oliveira@yahoo.com.br>
2019-11-12 09:34:59 -0300

    Config .env

--- server/configs/keys.config.js
+++ server/configs/keys.config.js
@@ -3,8 +3,8 @@ module.exports =
 	db : process.env.DB || "mongodb://localhost:27017/google-books-api",
 	jwt:
 	{
-		key	   : process.env.jwtKey || "@jwtKey",
-		secrect: process.env.jwtSecret || "@jwtSecret"
+		key	   : process.env.JwtKey || "@jwtKey",
+		secrect: process.env.JwtSecret || "@jwtSecret"
 
 	}
 
